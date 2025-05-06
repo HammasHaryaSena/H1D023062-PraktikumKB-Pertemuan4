ee22390a1731516f4634538af5c36246da262168
HammasHaryaSena <162069949+HammasHaryaSena@users.noreply.github.com>
2025-05-06 20:54:04 +0700

    Update percobaan2.pl

--- percobaan2.pl
+++ percobaan2.pl
@@ -8,21 +8,21 @@
 % -- yang berarti X mengidap penyakit Y.
 % -- Predikat gejala/2 berarti X memiliki gejala Y.
 penyakit(X, tertiana) :-
-    gejala(X, nyeri_otot),
-    gejala(X, muntah),
-    gejala(X, kejang).
+gejala(X, nyeri_otot),
+gejala(X, muntah),
+gejala(X, kejang).
 penyakit(X, quartana) :-
-    gejala(X, nyeri_otot),
-    gejala(X, menggigil),
-    gejala(X, tidak_enak_badan).
+gejala(X, nyeri_otot),
+gejala(X, menggigil),
+gejala(X, tidak_enak_badan).
 penyakit(X, tropika) :-
-    gejala(X, keringat_dingin),
-    gejala(X, sakit_kepala),
-    gejala(X, mimisan),
-    gejala(X, mual).
+gejala(X, keringat_dingin),
+gejala(X, sakit_kepala),
+gejala(X, mimisan),
+gejala(X, mual).
 penyakit(X, pernisiosa) :-
-    gejala(X, menggigil),
-    gejala(X, tidak_enak_badan),
-    gejala(X, demam),
-    gejala(X, mimisan),
-    gejala(X, mual).
+gejala(X, menggigil),
+gejala(X, tidak_enak_badan),
+gejala(X, demam),
+gejala(X, mimisan),
+gejala(X, mual).
