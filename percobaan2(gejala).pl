87fd0fff7db0a8c0d17f45a15d0373955c2dc404
HammasHaryaSena <162069949+HammasHaryaSena@users.noreply.github.com>
2025-05-06 20:33:51 +0700

    Update percobaan2.pl

--- percobaan2(gejala).pl
+++ percobaan2(gejala).pl
@@ -1,24 +1,28 @@
-% Fakta dan aturan deteksi gangguan mental pada anak
-
-gangguan(adhd) :-
-    gejala(sulit_fokus),
-    gejala(hiperaktif),
-    gejala(mudah_terdistraksi).
-
-gangguan(autisme) :-
-    gejala(perilaku_berulang),
-    gejala(kesulitan_interaksi).
-
-gangguan(depresi) :-
-    gejala(murung),
-    gejala(tidak_bersemangat),
-    gejala(menarik_diri).
-
-gangguan(kecemasan) :-
-    gejala(sering_cemas),
-    gejala(takut_berlebihan),
-    gejala(sulit_tidur).
-
-gangguan(ocd) :-
-    gejala(perilaku_kompulsif),
-    gejala(pikiran_obsesif).
+% GEJALA PENYAKIT MALARIA
+% DATABASE
+% -- Menyimpan data apakah X memiliki gejala Y.
+:- dynamic gejala/2.
+% ATURAN
+% -- Mengandung daftar gejala untuk beberapa jenis penyakit malaria,
+% -- dengan predikat penyakit/2 (menerima dua parameter)
+% -- yang berarti X mengidap penyakit Y.
+% -- Predikat gejala/2 berarti X memiliki gejala Y.
+penyakit(X, tertiana) :-
+    gejala(X, nyeri_otot),
+    gejala(X, muntah),
+    gejala(X, kejang).
+penyakit(X, quartana) :-
+    gejala(X, nyeri_otot),
+    gejala(X, menggigil),
+    gejala(X, tidak_enak_badan).
+penyakit(X, tropika) :-
+    gejala(X, keringat_dingin),
+    gejala(X, sakit_kepala),
+    gejala(X, mimisan),
+    gejala(X, mual).
+penyakit(X, pernisiosa) :-
+    gejala(X, menggigil),
+    gejala(X, tidak_enak_badan),
+    gejala(X, demam),
+    gejala(X, mimisan),
+    gejala(X, mual).
